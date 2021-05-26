eb24050a55b99a4ff787f490d36f8203c0229109
Eunice <eunicesimsingyee@gmail.com>
2021-05-26 21:18:36 +0800

    added confirm password

--- lib/SignUpPage.dart
+++ lib/SignUpPage.dart
@@ -32,17 +32,28 @@ class _SignUpPageState extends State<SignUpPage> {
                   decoration: InputDecoration(labelText: "Password "),
                   obscureText: true,
                   controller: _password,),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Confirm Password "),
+                  obscureText: true,
+                  controller: _confirmPassword,),
                 Container(height: 20),
                 TextFormField(decoration: InputDecoration(labelText: "Username: ")),
                 TextButton(
                     onPressed: () async {
-                      bool shouldRegister = await register(_email.text, _password.text);
-                      if (shouldRegister) {
-                        Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => LoginScreen(),)
-                        );
+                      if (_password.text == _confirmPassword.text) {
+                          bool shouldRegister = await register(_email.text, _password.text);
+                          if (shouldRegister) {
+                            Navigator.push(context,
+                                MaterialPageRoute(
+                                  builder: (context) => LoginScreen(),)
+                            );
+                            // else: pop-up – account alr registered under this email
+                          }
                       }
-
+                      // else : pop-up screen: confirm password does not match password
+                       else {
+                         print("Error");
+                       }
                     },
                     child: Text('Sign up'))
               ]
