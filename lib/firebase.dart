b0f117c24759d72fc17c145bdea3ccf8173e37b3
Shavon <shavontanqh@gmail.com>
2021-05-27 21:04:16 +0800

    add firebase.dart

---
+++ lib/firebase.dart
@@ -0,0 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<void> userSetup(String username, String email) async {
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+  User user = FirebaseAuth.instance.currentUser as User;
+  String uid = user.uid.toString();
+
+  users.add({
+    'uid': uid,
+    'username': username,
+    'email': email,
+    'points': 0,
+  });
+  return;
+}
