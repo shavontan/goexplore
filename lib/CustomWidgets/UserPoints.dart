fce133084faead71a30ab60db8d6a970b218f749
Eunice <eunicesimsingyee@gmail.com>
2021-05-28 23:25:05 +0800

    Added user's points tile

---
+++ lib/CustomWidgets/UserPoints.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class UserPoints extends StatefulWidget {
+  final int points;
+
+  const UserPoints({required this.points});
+
+  @override
+  _UserPointsState createState() => _UserPointsState();
+}
+
+class _UserPointsState extends State<UserPoints> {
+  @override
+  Widget build(BuildContext context) {
+    int pts = widget.points;
+
+    return SizedBox(
+        height: 150,
+        width: 300,
+        child: Card(
+          color: Color(0xFFFAEEDF,),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Stack(children: [
+            Positioned(
+              child: Text(
+                'You have',
+                style: GoogleFonts.delius(
+                  fontSize: 25,
+                ),
+              ),
+              top: 15,
+              left: 15,
+            ),
+            Positioned(
+              child: Text(
+                '$pts Points',
+                style: GoogleFonts.delius(
+                  fontSize: 45,
+                ),
+              ),
+              bottom: 15,
+              right: 15,
+            )
+          ]),
+        ));
+  }
+}
