8b11530f87acfcd7cdf8d249e57da3116a8fc9f4
Eunice <eunicesimsingyee@gmail.com>
2021-05-30 21:16:05 +0800

    Added Categories page

---
+++ lib/Categories.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Categories extends StatefulWidget {
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: Text('Category:', style: TextStyle(color: Colors.black)),
+            backgroundColor: Color(0xB6C4CAE8),
+            elevation: 0.0,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+            )),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                    height: 245,
+                    width: 300,
+                    child: InkWell(
+                        borderRadius: BorderRadius.circular(15.0),
+                        onTap: () {
+                          print("tapped");
+                        },
+                        child: Card(
+                            child: Stack(children: [
+                              Positioned.fill(
+                                child: Opacity(
+                                  child: Image.asset('assets/images/F&B.png'),
+                                  opacity: 0.4,
+                                ),
+                              ),
+                              Positioned(
+                                child: ConstrainedBox(
+                                  child: Text(
+                                    "Food & Beverages", // BUY 1 GET 1 FREE
+                                    style: GoogleFonts.reenieBeanie(
+                                      fontSize: 60,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  constraints:
+                                  BoxConstraints(maxWidth: 300, maxHeight: 200),
+                                ),
+                                top: 60,
+                                left: 5,
+                              ),
+                            ])))),
+                SizedBox(
+                    height: 245,
+                    width: 300,
+                    child: InkWell(
+                        borderRadius: BorderRadius.circular(15.0),
+                        onTap: () {
+                          print("tapped");
+                        },
+                        child: Card(
+                            child: Stack(children: [
+                              Positioned.fill(
+                                child: Opacity(
+                                  child: Image.asset('assets/images/Recreation.png'),
+                                  opacity: 0.4,
+                                ),
+                              ),
+                              Positioned(
+                                child: ConstrainedBox(
+                                  child: Text(
+                                    "Recreation", // BUY 1 GET 1 FREE
+                                    style: GoogleFonts.reenieBeanie(
+                                      fontSize: 60,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  constraints:
+                                  BoxConstraints(maxWidth: 300, maxHeight: 200),
+                                ),
+                                top: 90,
+                                left: 40,
+                              ),
+                            ])))),
+              ],
+            )));
+  }
+}
