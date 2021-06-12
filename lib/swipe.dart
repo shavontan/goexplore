30800d57a7fd1d1156bd44e9eaacc5627e18c571
Shavon <shavontanqh@gmail.com>
2021-06-12 20:58:37 +0800

    edit swipe

--- lib/swipe.dart
+++ lib/swipe.dart
@@ -6,10 +6,39 @@ import 'flutterfire.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import './profilepage.dart';
-
 import 'package:random_color/random_color.dart';
 import './bookmarksbar.dart';
+
+import 'package:geocoding/geocoding.dart' as gc;
+import 'package:geolocator/geolocator.dart';
+// // import 'package:geolocation/geolocation.dart';
+// // import 'package:location/location.dart';
+//
+// // beautiful popup imports
+// import 'package:flutter_beautiful_popup/main.dart';
+// import 'package:flutter_beautiful_popup/templates/Authentication.dart';
+// import 'package:flutter_beautiful_popup/templates/BlueRocket.dart';
+// import 'package:flutter_beautiful_popup/templates/Camera.dart';
+// import 'package:flutter_beautiful_popup/templates/Coin.dart';
+// import 'package:flutter_beautiful_popup/templates/Common.dart';
+// import 'package:flutter_beautiful_popup/templates/Fail.dart';
+// import 'package:flutter_beautiful_popup/templates/Geolocation.dart';
+// import 'package:flutter_beautiful_popup/templates/Gift.dart';
+// import 'package:flutter_beautiful_popup/templates/GreenRocket.dart';
+// import 'package:flutter_beautiful_popup/templates/Notification.dart';
+// import 'package:flutter_beautiful_popup/templates/OrangeRocket.dart';
+// import 'package:flutter_beautiful_popup/templates/OrangeRocket2.dart';
+// import 'package:flutter_beautiful_popup/templates/RedPacket.dart';
+// import 'package:flutter_beautiful_popup/templates/Success.dart';
+// import 'package:flutter_beautiful_popup/templates/Term.dart';
+// import 'package:flutter_beautiful_popup/templates/Thumb.dart';
+
+import 'package:huawei_location/location/fused_location_provider_client.dart';
+import 'package:huawei_location/location/location.dart';
+import 'package:huawei_location/location/location_request.dart';
+import 'package:huawei_location/permission/permission_handler.dart';
+
+
 // Link to DB
 
 class Swipe extends StatefulWidget {
@@ -17,11 +46,12 @@ class Swipe extends StatefulWidget {
   final String category;
   final int price;
   final List<String> tags;
+  final double dist;
 
-  Swipe(this.category, this.price, this.tags);
+  Swipe(this.category, this.price, this.tags, this.dist);
 
   @override
-  _SwipeState createState() => _SwipeState(this.category, this.price, this.tags);
+  _SwipeState createState() => _SwipeState(this.category, this.price, this.tags, this.dist);
 }
 
 
@@ -33,8 +63,9 @@ class _SwipeState extends State<Swipe> {
   final String category;
   final int price;
   final List<String> tags;
+  final double dist;
 
-  _SwipeState(this.category, this.price, this.tags);
+  _SwipeState(this.category, this.price, this.tags, this.dist);
 
   List<_Card> _Cards = [];
 
@@ -47,23 +78,15 @@ class _SwipeState extends State<Swipe> {
 
         // Important to keep as a stack to have overlay of _Cards.
 
-        child: StreamBuilder<QuerySnapshot>(
-            stream: getLocationStreamSnapshots(context, this.category, this.price, this.tags),
+        child: FutureBuilder<List<QueryDocumentSnapshot>>(
+            future: getLocationStreamSnapshots(context, this.category, this.price, this.tags, this.dist),
             builder: (context, snapshot) {
 
               if (!snapshot.hasData) {
                 return Center(child: CircularProgressIndicator(),);
               }
 
-              List<dynamic> filtered = snapshot.data!.docs.where((d) =>
-                  check(d['tags'], tags)
-              ).toList();
-
-              if (filtered.isEmpty) {
-                return Center(child: CircularProgressIndicator(),);
-              }
-
-              final randomDocs = filtered..shuffle();
+              final randomDocs = snapshot.data!..shuffle();
               final length = randomDocs.length;
 
               for (int i = 0; i < length; i++) {
@@ -170,6 +193,7 @@ class _Card extends StatelessWidget {
           ),
         ),
         onSwipeDown: (finalPosition) async {
+
           List<dynamic> bookmarks = await getBookmarks();
           String uid = await getCurrentUID();
           final List<bool> isSelected = globalKey.currentState!.isSelected;
@@ -199,17 +223,65 @@ class _Card extends StatelessWidget {
   }
 }
 
-Stream<QuerySnapshot> getLocationStreamSnapshots(
-    BuildContext context, String category, int price, List<String> tags) async* {
+// Stream<QuerySnapshot> getLocationStreamSnapshots(
+//     BuildContext context, String category, int price, List<String> tags) async* {
+//
+//   yield* FirebaseFirestore.instance
+//       .collection(category)
+//       .where('price', isLessThanOrEqualTo: price)
+//       .snapshots();
+//
+// }
 
-  yield* FirebaseFirestore.instance
+Future<List<QueryDocumentSnapshot>> getLocationStreamSnapshots(
+    BuildContext context, String category, int price, List<String> tags, double dist) async {
+
+  QuerySnapshot qs = await FirebaseFirestore.instance
       .collection(category)
       .where('price', isLessThanOrEqualTo: price)
-      .snapshots();
+      .get();
+  //
+  // Stream<QueryDocumentSnapshot> docsStream = Stream.fromIterable(qs
+  //     .docs
+  //     .where((d) => checkTags(d['tags'], tags)));
+
+  Iterable<QueryDocumentSnapshot> docsStream = qs
+      .docs
+      .where((d) => checkTags(d['tags'], tags));
+
+  // Init PermissionHandler
+  bool status;
+  PermissionHandler permissionHandler = PermissionHandler();
+  // Request location permissions
+  status = await permissionHandler.requestLocationPermission();
+
+  if (!status) {
+    return docsStream.toList();
+  }
+
+  FusedLocationProviderClient locationService = FusedLocationProviderClient();
+  LocationRequest locationRequest = new LocationRequest();
+  locationRequest.numUpdates = 1;
+
+  await locationService.requestLocationUpdates(locationRequest);
+  Location curr = await locationService.getLastLocation();
+  double lat = curr.latitude;
+  double long = curr.longitude;
+
+
+  List<QueryDocumentSnapshot> docsList = [];
+  for (QueryDocumentSnapshot d in docsStream) {
+    if(await checkDist(d, dist, status, lat, long)) {
+      docsList.add(d);
+    }
+  }
+
+
+  return docsList;
 
 }
 
-bool check(List<dynamic> doc, List<String> tag) {
+bool checkTags(List<dynamic> doc, List<String> tag) {
 
   if (tag.every((item) => doc.contains(item)) && doc.length > 0 || tag.length == 0) {
     return true;
@@ -217,4 +289,14 @@ bool check(List<dynamic> doc, List<String> tag) {
     return false;
   }
 }
+
+Future<bool> checkDist(QueryDocumentSnapshot doc, double requiredDist, bool status, double lat, double long) async {
+
+  List<gc.Location> locations = await gc.locationFromAddress(doc['address']);
+  double distanceInKM = Geolocator.distanceBetween(lat, long, locations[0].latitude, locations[0].longitude) * 0.001;
+  return distanceInKM < requiredDist;
+
+}
+
+
 
